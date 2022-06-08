9f28242794b4bedeb91233931e9da82b4e462997
Andreas Leonardo Soenarto <andreasleonardo.al@gmail.com>
2022-06-08 19:46:17 +0700

    tryerror

--- testapiflask.py
+++ testapiflask.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def matching():
-    df1 = pd.read_excel(r"E:\SKRIPSI\bananaapps\testapi\UserAndreGorLokasSari")
-    df2 = pd.read_excel(r"E:\SKRIPSI\bananaapps\testapi\UserDatabase")
+    df = pd.read_excel(r"E:\SKRIPSI\bananaapps\testapi\UserAndreGorLokasSari.xlsx")
+    df1 = pd.read_excel(r"E:\SKRIPSI\bananaapps\testapi\UserDatabase.xlsx")
     indexer = rl.Index()
     indexer.full()
-    pairs = indexer.index(df1,df2)
+    pairs = indexer.index(df,df1)
     compare_cl = rl.Compare()
     compare_cl.exact("SportType","SportType", label="SportTypePoint")
     compare_cl.numeric("Age","Age",scale=3,label="AgePoint")
     compare_cl.geo("X","Y","X","Y", method='exp', label="DistancePoint")
-    features = compare_cl.compute(pairs, df1, df2)
+    features = compare_cl.compute(pairs, df, df1)
     clusterone = features[features.AgePoint > 0.4]
     clustertwo = clusterone[clusterone.SportTypePoint > 0]
     clusterfinal = clustertwo[clustertwo.DistancePoint > 0.09]
