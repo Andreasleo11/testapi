4f8af653bd9c914cfe25a220f575b0bad2b65314
Andreas Leonardo Soenarto <andreasleonardo.al@gmail.com>
2022-06-14 14:19:45 +0700

    reset

--- testapiflask.py
+++ testapiflask.py
@@ -32,28 +32,8 @@ def matching():
     return pl
 
 @app.route("/posts")
-def matching():
-    df = pd.read_excel('UserAndreGorLokasSari.xlsx')
-    df1 = pd.read_excel('UserDatabase.xlsx')
-    indexer = rl.Index()
-    indexer.full()
-    pairs = indexer.index(df,df1)
-    compare_cl = rl.Compare()
-    compare_cl.exact("SportType","SportType", label="SportTypePoint")
-    compare_cl.numeric("Age","Age",scale=3,label="AgePoint")
-    compare_cl.geo("X","Y","X","Y", method='exp', label="DistancePoint")
-    features = compare_cl.compute(pairs, df, df1)
-    clusterone = features[features.AgePoint > 0.4]
-    clustertwo = clusterone[clusterone.SportTypePoint > 0]
-    clusterfinal = clustertwo[clustertwo.DistancePoint > 0.2]
-    ecm = rl.ECMClassifier(binarize=0)
-    match = ecm.fit_predict(clusterfinal)
-    # result = match.to_series().apply(lambda x: '{0}-{1}'.format(*x))
-    result = list(match)
-    resulttup = tuple(result)
-    # pl = ','.join(''.join(x) for x in resulttup)
-    pl = ','.join(map(str, resulttup))
-    return pl
+def hello_world():
+   return "hello world"
 
 if __name__ == "__main__":
     app.run()
