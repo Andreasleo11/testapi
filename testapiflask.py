8a1b7b6ec171e81034fd5ac9196f3ce23d04afca
Andreas Leonardo Soenarto <andreasleonardo.al@gmail.com>
2022-06-22 23:34:41 +0700

    wadawdwadawdawda

--- testapiflask.py
+++ testapiflask.py
@@ -38,33 +38,31 @@ def hello_world():
 
 @app.route("/posts", methods=['POST'])
 def matching():
-    print(request.get_json())
+    # print(request.get_json())
     data = request.get_json()
-    df = pd.read_json(data[0])
-    # df1 = pd.read_json()
-    # df = pd.read_excel('UserAndreGorLokasSari.xlsx')
-    # df1 = pd.read_excel('UserDatabase.xlsx')
-    # indexer = rl.Index()
-    # indexer.full()
-    # pairs = indexer.index(df,df1)
-    # compare_cl = rl.Compare()
-    # compare_cl.exact("SportType","SportType", label="SportTypePoint")
-    # compare_cl.numeric("Age","Age",scale=3,label="AgePoint")
-    # compare_cl.geo("X","Y","X","Y", method='exp', label="DistancePoint")
-    # features = compare_cl.compute(pairs, df, df1)
-    # clusterone = features[features.AgePoint > 0.4]
-    # clustertwo = clusterone[clusterone.SportTypePoint > 0]
-    # clusterfinal = clustertwo[clustertwo.DistancePoint > 0.2]
-    # ecm = rl.ECMClassifier(binarize=0)
-    # match = ecm.fit_predict(clusterfinal)
-    # # result = match.to_series().apply(lambda x: '{0}-{1}'.format(*x))
-    # rematch = match.get_level_values(1)
-    # rematch = list(rematch)
-    # resulttup = tuple(rematch)
-    # # pl = ','.join(''.join(x) for x in resulttup)
-    # pl = ','.join(map(str, resulttup))
-    # return jsonify(pl)
-    return df
+    inputdata = pd.DataFrame(data)
+    df = inputdata.head(1)
+    df1 = inputdata.iloc[1:]
+    indexer = rl.Index()
+    indexer.full()
+    pairs = indexer.index(df,df1)
+    compare_cl = rl.Compare()
+    compare_cl.exact("SportType","SportType", label="SportTypePoint")
+    compare_cl.numeric("Age","Age",scale=3,label="AgePoint")
+    compare_cl.geo("X","Y","X","Y", method='exp', label="DistancePoint")
+    features = compare_cl.compute(pairs, df, df1)
+    clusterone = features[features.AgePoint > 0.4]
+    clustertwo = clusterone[clusterone.SportTypePoint > 0]
+    clusterfinal = clustertwo[clustertwo.DistancePoint > 0.2]
+    ecm = rl.ECMClassifier(binarize=0)
+    match = ecm.fit_predict(clusterfinal)
+    # result = match.to_series().apply(lambda x: '{0}-{1}'.format(*x))
+    rematch = match.get_level_values(1)
+    rematch = list(rematch)
+    resulttup = tuple(rematch)
+    # pl = ','.join(''.join(x) for x in resulttup)
+    pl = ','.join(map(str, resulttup))
+    return jsonify(pl)
 
 
 
