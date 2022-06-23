6bda7e0c78fddcdd08b21db44b3fa2e873610d90
Andreas Leonardo Soenarto <andreasleonardo.al@gmail.com>
2022-06-24 01:53:00 +0700

    wadawdasdaw

--- testapiflask.py
+++ testapiflask.py
@@ -62,8 +62,8 @@ def matching():
     resulttup = tuple(rematch)
     # pl = ','.join(''.join(x) for x in resulttup)
     pl = ','.join(map(str, resulttup))
-    # return jsonify(pl)
-    return pl
+    return jsonify(pl)
+
 
 
 
